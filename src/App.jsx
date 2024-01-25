6bfaad841e8a5c942458652c1484ffecab473edd
Arinola23 <oladipomaryam8@gmail.com>
2024-01-25 21:15:52 +0100

    JSX And React Components Checkpoint

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Description from './components/description'
+import Image from './components/image'
+import Name from './components/name'
+import Price from './components/price'
+import Products from './Products/products'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Description/>
+      <Image/>
+      <Name/>
+      <Price/>
+      <Products/>
+    </>
+  )
+}
+
+export default App
